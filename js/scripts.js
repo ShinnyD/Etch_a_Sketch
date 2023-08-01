19c5fc3c087171836b194c25c50e9e71ed87ae1d
ShinnyD <shinobuduerloo@gmail.com>
2023-08-01 11:23:02 +0200

    creates dynamic grid depending on sizeOfGrid and adds mouseover Eventlister to change background color from white to black

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,40 @@
 const container = document.querySelector("#container");
+let sizeOfGrid = 16
 
-let gridCount = 0;
-while (gridCount < 32){
-    const gridDivs = document.createElement("div");
-    gridDivs.classList.add("content");
-    gridDivs.textContent = "Hello World!";
-    container.appendChild(gridDivs);
-    gridCount++;
-    console.log(gridCount);
-}
-
-/*function createGrid (boxNumber){
+function createGrid (boxNumber){
     for (let i = 0; i < boxNumber; i++){
-            const row = container.appendChild(document.createElement("div"))
-            console.log(boxNumber);
+            const row = document.createElement("div");
+            row.classList.add("gridRow")
             for (let j = 0; j < boxNumber; j++){
-                const box = document.createElement("div");
-                box.className = "box";
-                row.appendChild(box);
-                console.log(boxNumber);
+                const widthAndHeight = 960 / sizeOfGrid
+                const gridBox = document.createElement("div");
+                gridBox.classList.add("gridBox")
+                gridBox.style.width = `${widthAndHeight}px`;
+                gridBox.style.height = `${widthAndHeight}px`;
+
+                gridBox.addEventListener("mouseenter", () => {
+                    gridBox.style.backgroundColor = "black";
+                })
+
+                row.appendChild(gridBox);
             }
+        container.appendChild(row);
     }
 }
 
-createGrid(15);*/
+createGrid(sizeOfGrid);
+
+
+/*function makeRows (rows, columns){
+    for (let i = 0; i < rows; i++)
+}*/
+
+/*let gridCount = 0;
+while (gridCount < 256){
+    const gridDivs = document.createElement("div");
+    gridDivs.classList.add("content");
+    gridDivs.textContent = "Box";
+    container.appendChild(gridDivs);
+    gridCount++;
+    console.log(gridCount);
+}*/
